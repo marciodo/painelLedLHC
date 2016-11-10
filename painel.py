b8d301b6394be0f6531d0a430678b5937f3b9e53
marciodo <marciodo@gmail.com>
2016-11-10 21:59:40 -0200

    Starting to create some classes. Code does not work, yet.

--- painel.py
+++ painel.py
@@ -32,8 +32,43 @@ LHC = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
 
+SHIP = np.array([[0, 1, 0],
+                 [1, 1, 1]])
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+INVASOR = np.array([[1, 1],
+                    [1, 1]])
+
+BULLET = np.array([[1]])
+
+class Sprite():
+    def __init__(self):
+        self.drawing = []
+        # Position of left up pixel
+        self.position = (0, 0)
+        # RGB color, 8 bits each
+        self.color = [0, 0, 0]
+        self.visible = True
+
+class LedPanelLHC():
+    def __init__(self):
+        self.spriteList = []
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def addSprite(self, sprite):
+        self.spriteList.append(sprite)
+
+    def draw(self):
+        frame = [[[0, 0, 0] for a in range(10)] for b in range(10)]
+        for sprite in self.spriteList:
+
+
+
+'''
+>>> import numpy as np
+>>> frame = np.array([[[a, b, a+b] for a in range(10)] for b in range(10)])
+>>> frame[0:2,0:2]=[[[111,111,111],[222,222,222]],[[333,333,333],[444,444,444]]]
+'''
+
 
 a = 0
 fullFrame = LHC
